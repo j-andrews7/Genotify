58e405a0c6b6483b2e69904bb3c150668dad3ae9
Jared Andrews <jared.andrews07@gmail.com>
2017-11-02 17:09:43 -0500

    layout changes, begin UI fleshout
    Broken data hiding

--- js/query.js
+++ js/query.js
@@ -1,70 +1,90 @@
 (function() {
-        document.addEventListener('DOMContentLoaded', function(event) {
-            console.log('DOM fully loaded.');
-            document.getElementById('searchButton').addEventListener('click', newQuery);
-        });
+    document.addEventListener('DOMContentLoaded', function(event) {
+        console.log('DOM fully loaded.');
+        document.getElementById('searchButton').addEventListener('click', newQuery);
+    });
 
-        function newQuery() {
-            var term = document.getElementById('query').value;
-            var queryUrl = 'https://mygene.info/v3/query?q=' + term;
-            var speciesOptions = document.getElementById('species');
+    function newQuery() {
+        var term = document.getElementById('query').value;
+        var queryUrl = 'https://mygene.info/v3/query?q=' + term;
+        var speciesOptions = document.getElementById('species');
 
-            if (speciesOptions.selectedIndex !== -1) {
-                var species = speciesOptions.options[speciesOptions.selectedIndex].value;
-                queryUrl = 'https://mygene.info/v3/query?q=' + term + '&species=' + species;
-                console.log(species);
+        if (speciesOptions.selectedIndex !== -1) {
+            var species = speciesOptions.options[speciesOptions.selectedIndex].value;
+            queryUrl = 'https://mygene.info/v3/query?q=' + term + '&species=' + species;
+        }
+
+        fetch(queryUrl)
+            .then(
+                function(response) {
+                    if (response.status !== 200) {
+                        console.log('Looks like there was a problem. Status Code: ' +
+                            response.status);
+                        return;
+                    }
+
+                    response.json().then(function(data) {
+                        console.log(data);
+                        if (data.total !== 0 && data.success !== false) {
+                            topHit = data.hits[0]
+                            var basics = { 'geneSymbol': topHit.symbol, 'geneName': topHit.name, 'geneId': topHit._id, 'matchScore': topHit._score };
+                            displayData(basics)
+                            annotateGene(basics.geneId);
+                        } else {
+                            document.getElementById('basics').textContent = 'No hits.';
+                            document.getElementById('summary').textContent = '';
+                        }
+                    });
+                }
+            )
+            .catch(function(err) {
+                console.error('Fetch Query Error', err);
+            });
+    }
+
+    function displayData(dataArray) {
+        for (data in dataArray) {
+            console.log(data)
+            currentData = document.getElementById(data);
+            currentData.classList.remove('hidden');
+            currentLabel = document.getElementById(data + 'Label');
+            currentLabel.classList.remove('hidden');
+            currentData.textContent = dataArray[data]
+
+            hideData(document.getElementById("info"))
+        }
+    }
+
+    function hideData(divObj) {
+        // Hide all data in child nodes of givin div element.
+        var children = divObj.querySelectorAll("label");
+        for (child in children) {
+            if (!(child.style.classList.contains('hidden'))) {
+                child.style.classList.add('hidden');
             }
-
-            fetch(queryUrl)
-                .then(
-                    function(response) {
-                        if (response.status !== 200) {
-                            console.log('Looks like there was a problem. Status Code: ' +
-                                response.status);
-                            return;
-                        }
-
-                        response.json().then(function(data) {
-                            console.log(data);
-                            if (data.total !== 0 && data.success !== false) {
-                                top_hit = data.hits[0]
-                                var symbol = top_hit.symbol
-                                var geneId = top_hit._id
-                                var geneName = top_hit.name
-                                document.getElementById('basics').textContent = 'Official Gene Symbol: ' + symbol + '; Official Gene Name: ' + geneName + '; Entrez ID: ' + geneId;
-                                annotateGene(geneId);
-                            } else {
-                                document.getElementById('basics').textContent = 'No hits.'
-                                document.getElementById('summary').textContent = ''
-                            }
-                        });
-                    }
-                )
-                .catch(function(err) {
-                    console.error('Fetch Query Error', err);
-                });
         }
+    }
 
-        function annotateGene(gene) {
-            fetch('https://mygene.info/v3/gene/' + gene)
-                .then(
-                    function(response) {
-                        if (response.status !== 200) {
-                            console.log('Looks like there was a problem. Status Code: ' +
-                                response.status);
-                            return;
-                        }
-
-                        // Examine the text in the response
-                        response.json().then(function(data) {
-                            console.log(data);
-                            var summary = data.summary
-                            document.getElementById('summary').textContent = 'Gene Summary: ' + summary;
-                        });
+    function annotateGene(gene) {
+        fetch('https://mygene.info/v3/gene/' + gene)
+            .then(
+                function(response) {
+                    if (response.status !== 200) {
+                        console.log('Looks like there was a problem. Status Code: ' +
+                            response.status);
+                        return;
                     }
-                )
-                .catch(function(err) {
-                    console.error('Fetch Gene Annotation Error', err);
-                });
-        }
-    })();
+
+                    // Examine the text in the response
+                    response.json().then(function(data) {
+                        console.log(data);
+                        var summary = data.summary;
+                        document.getElementById('summary').textContent = 'Gene Summary: ' + summary;
+                    });
+                }
+            )
+            .catch(function(err) {
+                console.error('Fetch Gene Annotation Error', err);
+            });
+    }
+})();
